bfc88e6645196e6b94e57b523134dda2463d452b
Tuan Hoang <tuan.hoang@solislab.com>
2025-12-21 14:02:40 +0700

    update offline api

--- event.js
+++ event.js
@@ -1,90 +1,98 @@
 const fetchOptions = {
-    method: 'POST', headers: {
-        Authorization: 'Bearer 50291500:992e1f01-2451-4fc6-bed9-e7df5481e06d',
-        Checksum: '02afccf9b76c80b3ef2ac24832ac4cdbd0e5c75329d92b2ef45959c152b35605'
-    }
+  method: 'POST', headers: {
+    Authorization: 'Bearer 50291500:992e1f01-2451-4fc6-bed9-e7df5481e06d',
+    Checksum: '02afccf9b76c80b3ef2ac24832ac4cdbd0e5c75329d92b2ef45959c152b35605'
+  }
 };
 
 const fetchUrl = 'https://api.vietlott-sms.vn/mobile-api/customerAccount/getStatisticGbingoResult';
 
+const fetchOffUrl = 'https://bingo18.top/data/data.json';
+
 function handleGetOfflineTotal() {
-    const arr = getValues();
-    const counts = countTotal(arr, window.allResults);
+  const arr = getValues();
+
+  fetch(fetchOffUrl).then(res => res.json()).then(json => {
+    const counts = countTotal(arr, json.gbingoDraws);
     console.log(counts);
     displayResult(counts);
+  });
 }
 
 function handleGetOfflineTriple() {
-    const arr = getValues();
-    const counts = countTriple(arr, window.allResults);
+  const arr = getValues();
+
+  fetch(fetchOffUrl).then(res => res.json()).then(json => {
+    const counts = countTriple(arr, json.gbingoDraws);
     console.log(counts);
     displayResult(counts);
+  });
 }
 
 function handleGetOnlineTotal() {
-    const arr = getValues();
-    fetch(fetchUrl, fetchOptions).then(res => res.json()).then(json => {
-        const counts = countTotal(arr, json.gbingoDraws);
-        console.log(counts);
-        displayResult(counts);
-    });
+  const arr = getValues();
+  fetch(fetchUrl, fetchOptions).then(res => res.json()).then(json => {
+    const counts = countTotal(arr, json.gbingoDraws);
+    console.log(counts);
+    displayResult(counts);
+  });
 }
 
 async function handleGetOnlineTriple() {
-    const arr = getValues();
-    fetch(fetchUrl, fetchOptions).then(res => res.json()).then(json => {
-        const counts = countTriple(arr, json.gbingoDraws);
-        console.log(counts);
-        displayResult(counts);
-    });
+  const arr = getValues();
+  fetch(fetchUrl, fetchOptions).then(res => res.json()).then(json => {
+    const counts = countTriple(arr, json.gbingoDraws);
+    console.log(counts);
+    displayResult(counts);
+  });
 }
 
 function handleGetOnlineIndexStatistic() {
-    fetch(fetchUrl, fetchOptions).then(res => {
-        res.json().then(res => {
-            const results = res.gbingoDraws;
-            const arr = getValues();
-            const counts = countIndex(arr, results);
-            console.log(counts);
-            displayResult(counts);
-        });
+  fetch(fetchUrl, fetchOptions).then(res => {
+    res.json().then(res => {
+      const results = res.gbingoDraws;
+      const arr = getValues();
+      const counts = countIndex(arr, results);
+      console.log(counts);
+      displayResult(counts);
     });
+  });
 }
 
 function handleGetOfflineDouble() {
-    const arr = getValues();
-    const counts = countDouble(arr, allResults);
-    console.log(counts);
-    displayResult(counts);
+  const arr = getValues();
+  const counts = countDouble(arr, allResults);
+  console.log(counts);
+  displayResult(counts);
 }
 
 function getValues() {
-    if (!document.querySelector('#check-value').value) return [];
-    return document.querySelector('#check-value').value.split(' ').map((item) => parseInt(item));
+  if (!document.querySelector('#check-value').value) return [];
+  return document.querySelector('#check-value').value.split(' ').map((item) => parseInt(item));
 }
 
 function handleGetOfflineStatistic() {
-    displayResult(getStatistic(window.allResults));
-    console.log(getStatistic(window.allResults));
+  displayResult(getStatistic(window.allResults));
+  console.log(getStatistic(window.allResults));
 }
 
 function createForm() {
-    createAllResultForm();
-    createIframe();
+  createAllResultForm();
+  createIframe();
 }
 
 async function handleGetOnlineStatistic() {
-    const allResults = await getOfflineData();
-    displayResult(getStatistic(allResults));
-    console.log(getStatistic(allResults));
+  const allResults = await getOfflineData();
+  displayResult(getStatistic(allResults));
+  console.log(getStatistic(allResults));
 }
 
 function displayResult(result) {
-    const element = document.querySelector('#result');
-    element.innerHTML = JSON.stringify(result, null, 2);
+  const element = document.querySelector('#result');
+  element.innerHTML = JSON.stringify(result, null, 2);
 }
 
 async function getOfflineData() {
-    const data = await fetch('data.json');
-    return await data.json();
+  const data = await fetch('data.json');
+  return await data.json();
 }
